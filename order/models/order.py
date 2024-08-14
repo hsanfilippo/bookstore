d1f5f9ef940131c4c4773bcfc82863a3777ff303
hsanfilippo <henrique.sanfilippo@gmail.com>
2024-08-14 14:39:25 +0900

    [Config] Order e Product adicionados ao settings.py

--- order/models/order.py
+++ order/models/order.py
@@ -5,4 +5,3 @@ from django.contrib.auth.models import User
 class Order(models.Model):
     product = models.ManyToManyField(Product, blank=False)
     user = models.ForeignKey(User, null=False)
-    
