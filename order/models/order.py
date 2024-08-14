7898954e94843281a218c482464da2e778ee3bbe
hsanfilippo <henrique.sanfilippo@gmail.com>
2024-08-14 19:06:06 +0900

    [Config] Preparando os arquivos para a migração
    - Todos os modelos das apps devidamente registrados em seus respectivos 'admin.py';

--- order/models/order.py
+++ order/models/order.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User
 
 class Order(models.Model):
     product = models.ManyToManyField(Product, blank=False)
-    user = models.ForeignKey(User, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
